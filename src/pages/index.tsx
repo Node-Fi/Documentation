45e6f48bee7ba62cce5653c6846b93d85963989e
Dylan Mooers <dylmooers@gmail.com>
2023-01-11 08:43:39 -0600

    feat: landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 import styles from "./index.module.css";
 import Home from "../components/Home/Home";
+import { VerticalSelectableRow } from "../components/SelectableRows/VerticalSelectableRows";
+import { SelectablePresentation } from "../components/SelectableRows/SelectablePresentation";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -41,7 +43,98 @@ export default function HomePage(): JSX.Element {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <Home />
+      <div
+        style={{
+          display: "flex",
+          width: "100vw",
+          alignItems: "center",
+          alignContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {/* <Home /> */}
+        <SelectablePresentation
+          presentationItems={[
+            {
+              mainImage: (
+                <div
+                  style={{ width: 300, height: 300, backgroundColor: "red" }}
+                />
+              ),
+              row: {
+                title: "Auth (Wallets)",
+                description:
+                  "Connect, create, or recover a wallet\n(EOA, MPC, etc)",
+                leftContent: (
+                  <div
+                    style={{ width: 100, height: 100, backgroundColor: "red" }}
+                  />
+                ),
+              },
+            },
+            {
+              mainImage: (
+                <div
+                  style={{ width: 300, height: 300, backgroundColor: "blue" }}
+                />
+              ),
+              row: {
+                title: "Read (Data)",
+                description:
+                  "Get current state, historical, and\nreal-tiem data (super fast)",
+                leftContent: (
+                  <div
+                    style={{ width: 100, height: 100, backgroundColor: "blue" }}
+                  />
+                ),
+              },
+            },
+            {
+              mainImage: (
+                <div
+                  style={{ width: 300, height: 300, backgroundColor: "green" }}
+                />
+              ),
+              row: {
+                title: "Write (Transact)",
+                description:
+                  "Payments, requests, lending, LPing,\nstaking, swapping, etc",
+                leftContent: (
+                  <div
+                    style={{
+                      width: 100,
+                      height: 100,
+                      backgroundColor: "green",
+                    }}
+                  />
+                ),
+              },
+            },
+            {
+              mainImage: (
+                <div
+                  style={{ width: 300, height: 300, backgroundColor: "orange" }}
+                />
+              ),
+              row: {
+                title: "Compliance",
+                description:
+                  "OFAC screening, on-chain user\nanalytics, and consulting",
+                leftContent: (
+                  <div
+                    style={{
+                      width: 100,
+                      height: 100,
+                      backgroundColor: "orange",
+                    }}
+                  />
+                ),
+              },
+            },
+          ]}
+          className={styles.presentation}
+        />
+      </div>
     </Layout>
   );
 }
